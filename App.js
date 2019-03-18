177ad99e8973ce8c66cf50dca9800e396b90cae9
José Manuel Báez <baezsoriano97@gmail.com>
2019-03-18 10:02:30 +0100

    Login screen

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, StatusBar, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+import { Input } from 'react-native-elements';
+import { LinearGradient, Font } from 'expo';
+import LoginBox from './components/loginBox';
+
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { foo: "bar" };
+    this.state = { foo: "bar", fontLoaded: false };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'montserrat-extra-bold': require('./assets/fonts/Montserrat-ExtraBold.otf'),
+    });
+
+    this.setState({fontLoaded: true});
   }
 
   handlePress = async() => {
@@ -22,32 +34,74 @@ export default class App extends React.Component {
   }
 
   render() {
-    console.log(this.state.hello);
-    if ( this.state.hello === undefined ) {
-      console.log('Rendering first');
-      return (
-        <View style={styles.container}>
-          <Text>This APK should be uploadable</Text>
-          <Button onPress={this.handlePress.bind(this)}
-                  title="HANDLE!"></Button>
-        </View>
-      );
-    } else {
-      console.log('Rendering last');
-      return (
-        <View style={styles.container}>
-          <Text>{this.state.hello}</Text>
-        </View>
-      )
-    }
+    return (
+      <View style={styles.container}>
+        <StatusBar backgroundColor="blue" barStyle="light-content" />
+        <LoginBox></LoginBox>
+      </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#bee6ef',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  userInputContainer: {
+    marginTop: 20,
+    backgroundColor: '#c2c2c2'
+  },
+
+  userInput: {
+    padding:20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    width: 360,
+    fontFamily: 'montserrat-extra-bold',
+    color: 'black',
+    fontSize: 20
+  },
+
+  label: {
+    marginBottom: 10,
+    marginTop: 20,
+    marginLeft: 10,
+    fontFamily: 'montserrat-extra-bold',
+    color: 'white',
+    fontSize: 20
+  },
+
+  blueBox: {
+    padding: 20,
+    paddingTop: 8,
+    paddingBottom: 8,
+    borderRadius: 20,
+    elevation: 3
+  },
+
+  loginButton: {
+    padding: 20,
+    // backgroundColor: 'black',
+    borderRadius: 20,
+    marginLeft: 40,
+    marginRight: 40,
+    marginTop: 30,
+    marginBottom: 20,
+    // shadowOffset:{  width: 0,  height: 20,  },
+    // shadowColor: 'black',
+    // shadowOpacity: 1.0,
+    elevation: 3,
+    // shadowRadius: 10
+  },
+
+  buttonText: {
+    fontFamily: 'montserrat-extra-bold',
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center'
+  }
 });
